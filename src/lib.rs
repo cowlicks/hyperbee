381033e7db50bc72cb179cd740e48fbeffc3a168
Blake Griffith <blake.a.griffith@gmail.com>
2023-11-10 10:32:25 -0600

    Begin basic Hyperbee.get implementation

--- src/lib.rs
+++ src/lib.rs
@@ -3,15 +3,15 @@ pub mod messages {
 }
 
 use derive_builder::Builder;
-use hypercore::{Hypercore, HypercoreError};
+use hypercore::{Hypercore, HypercoreError, Info};
 use messages::{Node, YoloIndex};
 use prost::{bytes::Buf, DecodeError, EncodeError, Message};
 
-pub trait BytesLike: Into<Vec<u8>> + From<Vec<u8>> {}
-impl<T: Into<Vec<u8>> + From<Vec<u8>>> BytesLike for T {}
+pub trait BytesLike: Into<Vec<u8>> + From<Vec<u8>> + Clone + PartialEq {}
+impl<T: Into<Vec<u8>> + From<Vec<u8>> + Clone + PartialEq> BytesLike for T {}
 
-pub trait CoreMem: random_access_storage::RandomAccess + std::fmt::Debug {}
-impl<T: random_access_storage::RandomAccess + std::fmt::Debug> CoreMem for T {}
+pub trait CoreMem: random_access_storage::RandomAccess + std::fmt::Debug + Send {}
+impl<T: random_access_storage::RandomAccess + std::fmt::Debug + Send> CoreMem for T {}
 
 struct Key<T>
 where
@@ -146,6 +146,13 @@ impl<T: BytesLike, M: CoreMem> TreeNode<T, M> {
     fn preload(&self) {
         todo!()
     }
+    async fn get_key(&self, seq: u64) -> T {
+        todo!()
+    }
+
+    async fn get_block(&self, seq: u64) -> BlockEntry<T, M> {
+        todo!()
+    }
 }
 
 struct BlockEntry<T: BytesLike, M: CoreMem> {
@@ -167,6 +174,14 @@ impl<T: BytesLike, M: CoreMem> BlockEntry<T, M> {
             key: entry.key.into(),
             value: entry.value.map(|x| x.into()),
         }
+    }
+
+    fn is_target(&self, key: &T) -> bool {
+        key == &self.key
+    }
+
+    fn get_tree_node(offset: u64) -> TreeNode<T, M> {
+        todo!()
     }
 }
 
@@ -207,18 +222,17 @@ impl<M: CoreMem> Batch<M> {
 
 // TODO use builder pattern macros for Hyperbee opts
 #[derive(Debug, Builder)]
-#[builder(pattern = "owned")]
+#[builder(pattern = "owned", derive(Debug))]
 pub struct Hyperbee<M: CoreMem> {
     //this.core = core
-    core: Hypercore<M>,
-
+    pub core: Hypercore<M>,
     //this.keyEncoding = opts.keyEncoding ? codecs(opts.keyEncoding) : null
     // TODO make enum
-    key_encoding: String,
+    //key_encoding: String,
 
     //this.valueEncoding = opts.valueEncoding ? codecs(opts.valueEncoding) : null
     // TODO make enum
-    value_encoding: String,
+    //value_encoding: String,
 
     //this.extension = opts.extension !== false ? opts.extension || Extension.register(this) : null
     //this.metadata = opts.metadata || null
@@ -226,15 +240,15 @@ pub struct Hyperbee<M: CoreMem> {
 
     //this.sep = opts.sep || SEP
     // TODO set default
-    sep: String,
+    //sep: String,
 
     //this.readonly = !!opts.readonly
     // TODO set default false
-    readonly: bool,
+    //readonly: bool,
 
     //this.prefix = opts.prefix || null
     // TODO set default
-    prefix: String,
+    //prefix: String,
     //this._unprefixedKeyEncoding = this.keyEncoding
     //this._sub = !!this.prefix
     //this._checkout = opts.checkout || 0
@@ -259,47 +273,70 @@ pub struct Hyperbee<M: CoreMem> {
 }
 
 impl<M: CoreMem> Hyperbee<M> {
+    /// trying to duplicate Js Hb.versinon
+    pub fn version(&self) -> u64 {
+        self.core.info().length
+    }
     /// Gets the root of the tree
     pub async fn get_root<T: BytesLike>(&self, ensure_header: bool) -> TreeNode<T, M> {
+        let block = self.get_block(self.version() - 1).await;
         todo!()
     }
     pub async fn get_block<T: BytesLike>(&self, seq: u64) -> BlockEntry<T, M> {
         todo!()
     }
 
-    pub async fn get<T: BytesLike>(
-        &self,
-        key: T,
-        value: T,
-    ) -> Result<Option<Vec<u8>>, HypercoreError> {
+    pub async fn get<T: BytesLike>(&self, key: T) -> Result<Option<Vec<u8>>, HypercoreError> {
         //let node = await this.getRoot(false)
         let node = self.get_root::<T>(false).await;
         //while (true) {
-        //  if (node.block.isTarget(key)) {
-        //    return node.block.isDeletion() ? null : node.block.final(encoding)
-        //  }
+        loop {
+            //  if (node.block.isTarget(key)) {
+            //    return node.block.isDeletion() ? null : node.block.final(encoding)
+            //  }
+            if node.block.is_target(&key) {
+                return Ok(Some(node.block.index_buffer.clone().into()));
+            }
+            //
+            //  let s = 0
+            //  let e = node.keys.length
+            //  let c
+            let s = 0;
+            let e = node.keys.len();
+            /*
+            {
+                let c = while s < e {
+                    let mid = ((s + e) >> 1) as u64;
+                    // compare these
+                    let comp = key == node.get_key(mid).await;
+                    if comp == 0 {}
+                };
 
-        //  let s = 0
-        //  let e = node.keys.length
-        //  let c
+                if node.children.len() == 0 {
+                    return Ok(Option::None);
+                }
+                //let i = c < 0 ? e : s;
+            }
+            */
 
-        //  while (s < e) {
-        //    const mid = (s + e) >> 1
+            //  while (s < e) {
+            //    const mid = (s + e) >> 1
 
-        //    c = b4a.compare(key, await node.getKey(mid))
+            //    c = b4a.compare(key, await node.getKey(mid))
 
-        //    if (c === 0) return (await this.getBlock(node.keys[mid].seq)).final(encoding)
+            //    if (c === 0) return (await this.getBlock(node.keys[mid].seq)).final(encoding)
 
-        //    if (c < 0) e = mid
-        //    else s = mid + 1
-        //  }
+            //    if (c < 0) e = mid
+            //    else s = mid + 1
+            //  }
 
-        //  if (!node.children.length) return null
+            //  if (!node.children.length) return null
 
-        //  const i = c < 0 ? e : s
-        //  node = await node.getChildNode(i)
-        //}
-        todo!()
+            //  const i = c < 0 ? e : s
+            //  node = await node.getChildNode(i)
+            //}
+            todo!()
+        }
     }
 }
 
