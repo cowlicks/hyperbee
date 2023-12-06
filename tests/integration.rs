8fcd225ab8c12c558112accd092398e6d1dab724
Blake Griffith <blake.a.griffith@gmail.com>
2023-12-06 15:43:36 -0800

    More tests

--- tests/integration.rs
+++ tests/integration.rs
@@ -1,8 +1,6 @@
 use hyperbee_rs;
-use std::{
-    path::Path,
-    sync::{Arc, Mutex},
-};
+use std::{path::Path, sync::Arc};
+use tokio::sync::Mutex;
 
 use hypercore::{self, HypercoreBuilder, Storage};
 
@@ -13,12 +11,23 @@ async fn integration() -> Result<(), Box<dyn std::error::Error>> {
     let path = Path::new(&HYPERBEE_STORAGE_DIR).to_owned();
     let storage = Storage::new_disk(&path, false).await.unwrap();
     let mut hc = HypercoreBuilder::new(storage).build().await.unwrap();
-    let foo = hc.get(11).await.unwrap().unwrap();
-    dbg!(foo);
+    let _ = hc.get(11).await.unwrap().unwrap();
+
     let hc = Arc::new(Mutex::new(hc));
     let mut hb = hyperbee_rs::HyperbeeBuilder::default().core(hc).build()?;
-    //dbg!(hb.core.info());
     let buf = "0".as_bytes();
     let x = hb.get(buf.into()).await?.unwrap();
+    let x = std::str::from_utf8(&x).unwrap();
+    assert_eq!(x, "0");
+
+    let buf = "1".as_bytes();
+    let x = hb.get(buf.into()).await?.unwrap();
+    let x = std::str::from_utf8(&x).unwrap();
+    assert_eq!(x, "2");
+
+    let buf = "2".as_bytes();
+    let x = hb.get(buf.into()).await?.unwrap();
+    let x = std::str::from_utf8(&x).unwrap();
+    assert_eq!(x, "4");
     Ok(())
 }
