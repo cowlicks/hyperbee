b5862941c293deafb486f11ab105103b82599fd4
Blake Griffith <blake.a.griffith@gmail.com>
2024-02-13 16:55:36 -0600

    Make inf keys work for Vec<u8>

--- src/keys.rs
+++ src/keys.rs
@@ -1,6 +1,6 @@
 use std::{cmp::Ordering, fmt::Debug};
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum InfiniteKeys {
     Positive,
     Negative,
@@ -36,17 +36,56 @@ impl PartialOrd<InfiniteKeys> for [u8] {
         })
     }
 }
+impl PartialEq<Vec<u8>> for InfiniteKeys {
+    fn eq(&self, _other: &Vec<u8>) -> bool {
+        false
+    }
+}
+
+impl PartialEq<InfiniteKeys> for Vec<u8> {
+    fn eq(&self, _other: &InfiniteKeys) -> bool {
+        false
+    }
+}
+
+impl PartialOrd<Vec<u8>> for InfiniteKeys {
+    fn partial_cmp(&self, _other: &Vec<u8>) -> Option<std::cmp::Ordering> {
+        Some(match self {
+            Positive => Ordering::Greater,
+            Negative => Ordering::Less,
+        })
+    }
+}
+
+impl PartialOrd<InfiniteKeys> for Vec<u8> {
+    fn partial_cmp(&self, other: &InfiniteKeys) -> Option<Ordering> {
+        Some(match other {
+            Positive => Ordering::Less,
+            Negative => Ordering::Greater,
+        })
+    }
+}
 
 #[cfg(test)]
 mod test {
     use super::*;
     #[test]
-    fn test_inf() {
+    fn test_slice() {
         let a: Vec<u8> = vec![1, 2, 3];
         let b: &[u8] = &[5, 6, 7];
+        assert!(InfiniteKeys::Positive > a[..]);
+        assert!(InfiniteKeys::Positive > *b);
         assert!(a[..] < InfiniteKeys::Positive);
         assert!(*b < InfiniteKeys::Positive);
         assert!(a[..] >= InfiniteKeys::Negative);
         assert!(*b >= InfiniteKeys::Negative);
+    }
+    #[test]
+    fn test_vec() {
+        let a: Vec<u8> = vec![1, 2, 3];
+        assert!(InfiniteKeys::Positive > a);
+        assert!(InfiniteKeys::Negative < a);
+        assert!(a < InfiniteKeys::Positive);
+        assert!(a > InfiniteKeys::Negative);
     }
 }
