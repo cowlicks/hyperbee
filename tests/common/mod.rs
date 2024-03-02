c220e27e2294469a5c53cfae0f40f441b41ab9b0
Blake Griffith <blake.a.griffith@gmail.com>
2024-03-02 13:12:32 -0600

    Assert that hyperbee git returns right result

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -1,7 +1,7 @@
 // cargo thinks everything in here is unused even though it is used in the integration tests
 #![allow(dead_code)]
 
-use std::process::Command;
+use std::process::{Command, Output};
 pub mod c;
 pub mod js;
 
@@ -11,4 +11,11 @@ pub fn git_root() -> Result<String, Box<dyn std::error::Error>> {
         .arg("git rev-parse --show-toplevel")
         .output()?;
     Ok(String::from_utf8(x.stdout)?.trim().to_string())
+}
+
+pub fn run_script_relative_to_git_root(script: &str) -> Result<Output, Box<dyn std::error::Error>> {
+    Ok(Command::new("sh")
+        .arg("-c")
+        .arg(format!("cd {} && {}", git_root()?, script))
+        .output()?)
 }
--- tests/ffi.rs
+++ tests/ffi.rs
@@ -1,10 +1,12 @@
 mod common;
-use common::c::run_make_with;
+use common::{c::run_make_with, run_script_relative_to_git_root};
 
 #[test]
 fn make_test_basic_ffi_hb_get() -> Result<(), Box<dyn std::error::Error>> {
-    let output = run_make_with("test")?;
-    dbg!(&output);
+    let _ = run_make_with("target/hyperbee")?;
+    let output = run_script_relative_to_git_root("tests/common/c/target/hyperbee")?;
     assert!(output.status.success());
+    let stdout = String::from_utf8(output.stdout)?.trim().to_string();
+    assert_eq!(stdout, "25");
     Ok(())
 }
