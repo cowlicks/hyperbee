2ec120e9e684753ac1f6c6157d50e548b259d086
Blake Griffith <blake.a.griffith@gmail.com>
2024-03-15 15:53:09 -0500

    Add python test from_disk 0-100

--- tests/python.rs
+++ tests/python.rs
@@ -1,5 +1,10 @@
 mod common;
-use common::python::{require_python, run_python};
+use common::{
+    parse_json_result,
+    python::{require_python, run_python},
+    write_100,
+};
+use hyperbee::Hyperbee;
 
 #[tokio::test]
 async fn hello_world_get_set_del() -> Result<(), Box<dyn std::error::Error>> {
@@ -22,5 +27,31 @@ async def main():
     )?;
     dbg!(&out);
     assert_eq!(out.status.code(), Some(0));
+    Ok(())
+}
+
+#[tokio::test]
+async fn zero_to_one_hundred() -> Result<(), Box<dyn std::error::Error>> {
+    let _x = require_python()?;
+    let storage_dir = tempfile::tempdir()?;
+    let hb = Hyperbee::from_storage_dir(&storage_dir).await?;
+    let keys = write_100!(&hb);
+    let code = format!(
+        "
+async def main():
+    import json
+    hb = await hyperbee_from_storage_dir('{}')
+    keys = [bytes(str(i), 'utf8') for i in range(100)]
+    results = [await hb.get(k) for k in keys]
+    values = [res.value.decode('utf8') for res in results]
+    print(json.dumps(values))
+",
+        storage_dir.path().display()
+    );
+
+    let output = run_python(&code)?;
+    let res = parse_json_result(&output)?;
+    assert_eq!(res, keys);
+    assert_eq!(output.status.code(), Some(0));
     Ok(())
 }
