6a46e3df12fcfc4567dce4fb13bcb2041c5e3226
Blake Griffith <blake.a.griffith@gmail.com>
2024-03-19 13:51:51 -0500

    Add docs to uniffi functions

--- src/ffi.rs
+++ src/ffi.rs
@@ -30,8 +30,10 @@ impl Putten {
     }
 }
 
-/// NB: this shadows [`crate::Hyperbee`], but it is not exposed in the public Rust API. We use the
-/// same name because we want the FFI API to use the Hyperbee name too.
+/// An append only B-Tree built on [`Hypercore`](hypercore::Hypercore). It provides a key-value
+/// store API, with methods for [inserting](Hyperbee::put), [getting](Hyperbee::get), and
+/// [deleting](Hyperbee::del) key-value pair. As well as creating [sorted
+/// iterators](Hyperbee::traverse), and ["sub" B-Trees](Hyperbee::sub) for grouping related data.
 #[derive(Debug, uniffi::Object)]
 struct Hyperbee {
     rust_hyperbee: Shared<RustHyperbee>,
@@ -39,10 +41,15 @@ struct Hyperbee {
 
 #[uniffi::export]
 impl Hyperbee {
+    /// The number of blocks in the hypercore.
+    /// The first block is always the header block so:
+    /// `version` would be the `seq` of the next block
+    /// `version - 1` is most recent block
     pub async fn version(&self) -> u64 {
         self.rust_hyperbee.read().await.version().await
     }
 
+    /// Get the value corresponding to the provided `key` from the Hyperbee
     async fn get(&self, key: &[u8]) -> Result<Option<Gotten>, HyperbeeError> {
         Ok(self
             .rust_hyperbee
@@ -53,6 +60,9 @@ impl Hyperbee {
             .map(|(seq, value)| Gotten::new(seq, value)))
     }
 
+    /// Insert the given key and value into the tree
+    /// Returs the `seq` of the new key, and `Option<u64>` which contains the `seq` of the old key
+    /// if it was replaced.
     async fn put(&self, key: &[u8], value: Option<Vec<u8>>) -> Result<Putten, HyperbeeError> {
         let (old_seq, new_seq) = self
             .rust_hyperbee
@@ -63,12 +73,16 @@ impl Hyperbee {
         Ok(Putten::new(old_seq, new_seq))
     }
 
+    /// Delete the given key from the tree.
+    /// Returns the `seq` from the key if it was deleted.
     async fn delete(&self, key: &[u8]) -> Result<Option<u64>, HyperbeeError> {
         self.rust_hyperbee.read().await.del(key).await
     }
 
-    // It'd be nice to make this non-async
-    // TODO setting the seperator is not-obvious and should be documented
+    /// Create a new tree with all it's operation's prefixed by the provided `prefix`.
+    /// Requires a config object to define the seperator between the prefix and the key.
+    /// Create the config with [`default_sub_config`]. This provides gives a NULL bytes seperator,
+    /// which is the same value as the Holepunch JavaScript library.
     async fn sub(&self, prefix: &[u8], config: RustPrefixedConfig) -> Prefixed {
         let rust_prefixed = self.rust_hyperbee.read().await.sub(prefix, config);
         Prefixed {
@@ -77,6 +91,7 @@ impl Hyperbee {
     }
 }
 
+/// Helper for creating a Hyperbee that exists in RAM.
 #[uniffi::export]
 async fn hyperbee_from_ram() -> Result<Hyperbee, HyperbeeError> {
     let rust_hyperbee = RustHyperbee::from_ram().await?;
@@ -85,6 +100,7 @@ async fn hyperbee_from_ram() -> Result<Hyperbee, HyperbeeError> {
     })
 }
 
+/// Helper for creating a Hyperbee from the provided path to a storage directory
 #[uniffi::export]
 async fn hyperbee_from_storage_dir(path_to_storage_dir: &str) -> Result<Hyperbee, HyperbeeError> {
     let rust_hyperbee = RustHyperbee::from_storage_dir(path_to_storage_dir).await?;
@@ -100,6 +116,7 @@ struct Prefixed {
 
 #[uniffi::export]
 impl Prefixed {
+    /// Get the value associated with the provided key plus this [`Prefixed`]'s instance's `prefix`
     async fn get(&self, key: &[u8]) -> Result<Option<Gotten>, HyperbeeError> {
         Ok(self
             .rust_prefixed
@@ -110,6 +127,7 @@ impl Prefixed {
             .map(|(seq, value)| Gotten::new(seq, value)))
     }
 
+    /// Insert the value into the tree with the provided key plus this [`Prefixed`]'s instance's `prefix`
     async fn put(&self, key: &[u8], value: Option<Vec<u8>>) -> Result<Putten, HyperbeeError> {
         let (old_seq, new_seq) = self
             .rust_prefixed
@@ -120,11 +138,13 @@ impl Prefixed {
         Ok(Putten::new(old_seq, new_seq))
     }
 
+    /// Insert the provided key and it's value. Key is is Prefixed by this [`Prefixed`]'s instance's `prefix`
     async fn delete(&self, key: &[u8]) -> Result<Option<u64>, HyperbeeError> {
         self.rust_prefixed.read().await.del(key).await
     }
 }
 
+/// Create the default config for creating a [`Prefixed`] instance
 #[uniffi::export]
 fn default_sub_config() -> RustPrefixedConfig {
     RustPrefixedConfig::default()
