6d1c878297a7b326d62d89b9066bad32b0e7f47f
Blake Griffith <blake.a.griffith@gmail.com>
2024-03-20 11:36:34 -0500

    Add result type for tests

--- tests/ffi.rs
+++ tests/ffi.rs
@@ -1,10 +1,10 @@
 mod common;
-use common::{c::run_make_with, run_script_relative_to_git_root};
+use common::{c::run_make_with, run_script_relative_to_git_root, Result};
 
 use crate::common::c::require_c_exe;
 
 #[test]
-fn make_test_basic_ffi_hb_get() -> Result<(), Box<dyn std::error::Error>> {
+fn make_test_basic_ffi_hb_get() -> Result<()> {
     require_c_exe()?;
     let _ = run_make_with("target/hyperbee")?;
     let output = run_script_relative_to_git_root("tests/common/c/target/hyperbee")?;
