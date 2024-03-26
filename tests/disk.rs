7cf21eccbe471f44a721d12e4513b18febd7798d
Blake Griffith <blake.a.griffith@gmail.com>
2024-03-26 14:16:57 -0500

    wip disk compare with deletions

--- tests/disk.rs
+++ tests/disk.rs
@@ -7,7 +7,7 @@ use common::{check_cmd_output, js::run_js_writable, Result};
 
 use hyperbee::Hyperbee;
 
-use crate::common::write_range_to_hb;
+use crate::common::{i32_key_vec, write_range_to_hb, Rand};
 
 fn run_command(cmd: &str) -> Result<Output> {
     check_cmd_output(Command::new("sh").arg("-c").arg(cmd).output()?)
@@ -41,7 +41,7 @@ fn create_storage_dirs_with_same_keys() -> Result<(TempDir, TempDir)> {
 }
 
 #[tokio::test]
-async fn check_disk_hello_world() -> Result<()> {
+async fn compare_disk_hello_world() -> Result<()> {
     // set up the initial directories
     let (jsdir, rdir) = create_storage_dirs_with_same_keys()?;
     let jsdir_str = jsdir.path().display().to_string();
@@ -89,5 +89,61 @@ async fn compare_trees_of_some_ranges() -> Result<()> {
         }
         diff_dirs(&jsdir_str, &rdir_str)?;
     }
+    Ok(())
+}
+
+#[tokio::test]
+async fn foo_compare_disk_put_and_del() -> Result<()> {
+    // set up the initial directories
+    let (jsdir, rdir) = create_storage_dirs_with_same_keys()?;
+    let jsdir_str = jsdir.path().display().to_string();
+    let rdir_str = rdir.path().display().to_string();
+
+    // add hello world to rust
+    let rand = Rand::default();
+    let hb = Hyperbee::from_storage_dir(&rdir_str).await?;
+
+    let keys = (0..5).collect();
+    let keys = rand.shuffle(keys);
+
+    for key in keys.iter() {
+        let key = i32_key_vec(*key);
+        hb.put(&key, Some(&key)).await?;
+    }
+
+    // add keys
+    let code = format!(
+        "
+    const keys = {};
+    for (const ikey of keys) {{
+        const key = String(ikey);
+        await hb.put(key, key)
+    }}
+",
+        serde_json::to_string(&keys)?
+    );
+    let _ = run_js_writable(&jsdir_str, &code)?;
+    // compare directies
+    diff_dirs(&jsdir_str, &rdir_str)?;
+
+    // shuffle keys again and delete
+    let keys = rand.shuffle(keys);
+    for key in keys.iter() {
+        let key = i32_key_vec(*key);
+        hb.del(&key).await?;
+    }
+    let code = format!(
+        "
+    const keys = {};
+    for (const ikey of keys) {{
+        const key = String(ikey);
+        await hb.del(key);
+    }}
+",
+        serde_json::to_string(&keys)?
+    );
+    let _ = run_js_writable(&jsdir_str, &code)?;
+    // compare directies
+    diff_dirs(&jsdir_str, &rdir_str)?;
     Ok(())
 }
