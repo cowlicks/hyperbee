aec701b642633c8c6576cd505155f41654c1db6a
Blake Griffith <blake.a.griffith@gmail.com>
2024-03-29 14:56:42 -0500

    Sort blocks by depth first search

--- src/blocks.rs
+++ src/blocks.rs
@@ -11,7 +11,7 @@ use tracing::trace;
 use crate::{
     changes::Changes,
     messages::{Node as NodeSchema, YoloIndex},
-    BlockEntry, Child, HyperbeeError, Shared, SharedNode,
+    BlockEntry, HyperbeeError, Shared, SharedNode,
 };
 
 #[derive(Builder, Debug)]
@@ -99,36 +99,23 @@ impl Blocks {
 
     #[tracing::instrument(skip(self, changes))]
     /// Commit [`Changes`](crate::changes::Changes) to the Hypercore
-    // TODO create a BlockEntry from changes and add it to self.cache
     pub async fn add_changes(&self, changes: Changes) -> Result<AppendOutcome, HyperbeeError> {
         let Changes {
             key,
             value,
-            mut nodes,
+            nodes,
             seq,
             ..
         } = changes;
         trace!("Adding changes with # non-root nodes [{}]", nodes.len());
-        let mut new_nodes = vec![];
-        // Could # nodes be greater than u64? No way.
-        let n_nodes_in_block: u64 = nodes.len() as u64;
+        let reordered_nodes = reorder_nodes(seq, &nodes).await;
 
-        // re-order nodes to match js hyperbee
-        // and update their offset
-        let mut node_reorder = vec![];
-        node_reorder.append(&mut nodes);
-
-        node_reorder.reverse();
-        for node in node_reorder.into_iter() {
-            node.write()
-                .await
-                .children
-                .update_offsets(seq, n_nodes_in_block)
-                .await;
-            new_nodes.push(node.read().await.to_level().await);
+        let mut levels = vec![];
+        for node in reordered_nodes {
+            levels.push(node.read().await.to_level().await)
         }
 
-        let index = YoloIndex { levels: new_nodes };
+        let index = YoloIndex { levels };
 
         let mut index_buf = Vec::with_capacity(index.encoded_len());
         YoloIndex::encode(&index, &mut index_buf).map_err(HyperbeeError::YoloIndexEncodingError)?;
@@ -143,5 +130,92 @@ impl Blocks {
         NodeSchema::encode(&node_schema, &mut node_schema_buf)
             .map_err(HyperbeeError::NodeEncodingError)?;
         self.append(&node_schema_buf).await
+    }
+}
+
+/// Gets the references to the children for the provided `seq` from the given `node`.
+/// NB: we `.rev()` this because we're inserting these into a stack, and want children poped from
+/// the stack to be in order.
+async fn take_children_with_seq(node: &SharedNode, seq: u64) -> Vec<(SharedNode, usize)> {
+    node.read()
+        .await
+        .children
+        .children
+        .read()
+        .await
+        .iter()
+        .enumerate()
+        .filter(|(_i, c)| c.seq == seq)
+        .rev()
+        .map(|(i, _)| (node.clone(), i))
+        .collect()
+}
+
+/// To get the same on-disk binary data as JavaScript Hyperbee, we reorder the nodes.
+/// via a depth-first search.
+/// https://github.com/holepunchto/hyperbee/blob/e1b398f5afef707b73e62f575f2b166bcef1fa34/index.js#L237-L249
+async fn reorder_nodes(seq: u64, nodes: &Vec<SharedNode>) -> Vec<SharedNode> {
+    let root = &nodes[nodes.len() - 1];
+    let mut child_stack = vec![];
+    let mut out = vec![root.clone()];
+
+    child_stack.append(&mut take_children_with_seq(root, seq).await);
+
+    while !child_stack.is_empty() {
+        // Get the next child, update it's offset
+        let (node, child_index) = child_stack.pop().expect("checked by `.is_empty` above");
+        // The get the childs old offset, so we can get the node it points to
+        let old_offset = node.read().await.children.children.read().await[child_index].offset;
+        // The child's node is pushed into `out` so it's offset will be `out.len()`
+        node.read().await.children.children.write().await[child_index].offset = out.len() as u64;
+
+        let childs_node = nodes[old_offset as usize].clone();
+        // Push the child's node into the output
+        out.push(childs_node.clone());
+        // stage the child's nodes children to be reordered
+        child_stack.append(&mut take_children_with_seq(&childs_node, seq).await);
+    }
+    out
+}
+
+// NB: reverse_nodes was the way we originally reordered the nodes so that the root would be at
+// index 0.
+mod _unused {
+    #![allow(dead_code)]
+    use crate::SharedNode;
+
+    async fn reverse_nodes(seq: u64, nodes: &Vec<SharedNode>) -> Vec<SharedNode> {
+        let mut nodes = nodes.clone();
+        let mut out = vec![];
+        // Could # nodes be greater than u64? No way.
+        let n_nodes_in_block: u64 = nodes.len() as u64;
+
+        // re-order nodes to match js hyperbee
+        // and update their offset
+        let mut node_reorder = vec![];
+        node_reorder.append(&mut nodes);
+
+        node_reorder.reverse();
+        for node in node_reorder.into_iter() {
+            update_offsets(node.clone(), seq, n_nodes_in_block).await;
+            out.push(node);
+        }
+        out
+    }
+
+    async fn update_offsets(node: SharedNode, seq: u64, n_nodes_in_block: u64) {
+        for child in node
+            .write()
+            .await
+            .children
+            .children
+            .write()
+            .await
+            .iter_mut()
+        {
+            if child.seq == seq {
+                child.offset = n_nodes_in_block - child.offset - 1;
+            }
+        }
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -198,14 +198,6 @@ fn make_node_vec<B: Buf>(buf: B, blocks: Shared<Blocks>) -> Result<Vec<SharedNod
 }
 
 impl Children {
-    async fn update_offsets(&mut self, seq: u64, n_nodes_in_block: u64) {
-        for child in self.children.write().await.iter_mut() {
-            if child.seq == seq {
-                child.offset = n_nodes_in_block - child.offset - 1;
-            }
-        }
-    }
-
     fn new(blocks: Shared<Blocks>, children: Vec<Child>) -> Self {
         Self {
             blocks,
