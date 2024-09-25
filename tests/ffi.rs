7deb88436af912d67fdfd2faf6cb6f690b25aca6
Blake Griffith <blake.a.griffith@gmail.com>
2024-09-25 11:13:25 -0400

    Add CI

--- tests/ffi.rs
+++ tests/ffi.rs
@@ -5,10 +5,16 @@ use crate::common::c::require_c_exe;
 
 #[test]
 fn make_test_basic_ffi_hb_get() -> Result<()> {
+    //let x: &[u8] = &[128, 228, 64, 216, 240, 85, 50, 53, 10];
+    //println!("{}", String::from_utf8_lossy(&x));
+    //assert!(false);
     require_c_exe()?;
     let _ = run_make_with("target/hyperbee")?;
     let output = run_script_relative_to_git_root("tests/common/c/target/hyperbee")?;
-    assert!(output.status.success());
+    dbg!(&output.stdout);
+    println!("stdout:\n{}", String::from_utf8_lossy(&output.stdout));
+    dbg!(&output.stderr);
+    println!("stderr:\n{}", String::from_utf8_lossy(&output.stderr));
     let stdout = String::from_utf8(output.stdout)?.trim().to_string();
     assert_eq!(stdout, "25");
     Ok(())
